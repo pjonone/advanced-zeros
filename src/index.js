4f63041a7eff3659832bdc5296055f46f7bd5945
Sergey <44573680+pjonone@users.noreply.github.com>
2019-03-18 21:36:55 +0300

    Fix: repair bug with count and change var to let

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number, base) {
-    var arr = [];
-    var sumArr = [];
-    for (var i = 2; i <= base; i++) {
+    let arr = [];
+    let sumArr = [];
+    for (let i = 2; i <= base; i++) {
         while (base % i === 0) {
             arr.push(i);
             base /= i;
@@ -10,8 +10,8 @@ module.exports = function getZerosCount(number, base) {
             arr.push(base);
         }
     }
-    var z = Math.max.apply(null, arr);
-    for (var y = 1; y < 30; y++) {
+    let z = Math.max.apply(null, arr);
+    for (let y = 1; y < 30; y++) {
         var num = Math.pow(z, y);
         var result = Math.floor(number / num);
         if (result !== 0) sumArr.push(result);
@@ -19,15 +19,14 @@ module.exports = function getZerosCount(number, base) {
     let summ = sumArr.reduce(function (sum, current) {
         return sum + current;
     });
-    var count = 0;
-    for(var x = 0; x < arr.length; ++x){
-        if(arr[x] == z)
+    let count = 0;
+    for (let x = 0; x < arr.length; ++x) {
+        if (arr[x] == z)
             count++;
     }
     if (count > 1) {
-        return Math.floor(summ/count);
-    }
-    else {
+        return Math.floor(summ / count);
+    } else {
         return summ;
     }
 
